9328c5f40ab5cdc5f8b14fc263c1ec1479c97ed0
name <email@gmail.com>
2025-02-17 09:35:34 +0530

    server and client files are added to repo

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import connectDB from './config/connectDB.js'
+import userRouter from './route/user.route.js'
+
+const app = express()
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan())
+app.use(helmet({
+    crossOriginResourcePolicy: false
+}))
+
+const PORT = 2020 || process.env.PORT
+
+app.get("/", (request, response) => {
+    ///server to client
+    response.json({
+        message: "Server is running " + PORT
+    })
+})
+
+app.use('/api/user', userRouter)
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running", PORT)
+    })
+})
+
