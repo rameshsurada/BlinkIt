423a6e771ddad6c822ac99a28fdc9d05db6ac7a7
name <suradaramesh4@gmail.com>
2025-02-21 12:02:27 +0530

    register page updated

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -23,16 +23,25 @@ const Register = () => {
   console.log("data : ", data);
 
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
   const handlePasswordShow = (e) => {
     setShowPassword((pre) => !pre);
+    setShowConfirmPassword((pre) => !pre);
+  };
+
+  const checkValues = Object.values(data).every((e1) => e1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
   return (
     <section className="w-full container mx-auto px-2">
-      <div className="bg-white my-4 max-w-lg mx-auto rounded p-4">
-        <p> User Register Page</p>
+      <div className="bg-white my-4 max-w-lg mx-auto rounded p-7">
+        <p> Welcome to BlinkIt</p>
 
-        <form>
-          <div className="gird gap-2 mt-6">
+        <form className="grid gap-4 mt-6 " onSubmit={handleSubmit}>
+          <div className="gird gap-1">
             <label htmlFor="name"> Name : </label>
             <br></br>
             <input
@@ -40,14 +49,14 @@ const Register = () => {
               type="text"
               placeholder="Enter your name"
               autoFocus
-              className="bg-blue-50 p-2 w-full outline-none focus: border-x-primary-200"
+              className="bg-blue-50 p-2 w-full  rounded outline-none focus: border-x-primary-200"
               name="name"
               value={data.name}
               onChange={handleChange}
             ></input>
           </div>
 
-          <div className="gird gap-2 mt-6">
+          <div className="gird gap-1 ">
             <label htmlFor="email"> Email : </label>
             <br></br>
             <input
@@ -61,14 +70,14 @@ const Register = () => {
             ></input>
           </div>
 
-          <div className="gird gap-2 mt-6">
+          <div className="gird gap-1">
             <label htmlFor="password"> Password : </label>
             <br></br>
             <div className="bg-blue-50 p-2 border rounded flex items-center ">
               <input
                 id="password"
                 type={showPassword ? "text" : "password"}
-                className="bg-blue-50  w-full outline-none  focus-within:border-x-primary-200 "
+                className="bg-blue-50  w-full outline-none focus:border-primary-200 "
                 name="password"
                 value={data.password}
                 onChange={handleChange}
@@ -88,19 +97,40 @@ const Register = () => {
             </div>
           </div>
 
-          <div className="gird gap-2 mt-6">
+          <div className="gird gap-1">
             <label htmlFor="password"> Confirm Password : </label>
             <br></br>
-            <input
-              id="confirmPassword"
-              type="confirmPassword"
-              className="bg-blue-50 p-2 w-full outline-none focus-within:border-x-primary-200 "
-              name="confirmPassword"
-              value={data.confirmPassword}
-              onChange={handleChange}
-              placeholder="Confirm your password"
-            ></input>
+            <div className="bg-blue-50 p-2 border rounded flex items-center">
+              <input
+                id="confirmPassword"
+                type={showPassword ? "text" : "password"}
+                className="bg-blue-50  w-full outline-none focus-within:border-x-primary-200 "
+                name="confirmPassword"
+                value={data.confirmPassword}
+                onChange={handleChange}
+                placeholder="Confirm your password"
+              ></input>
+              {!showPassword ? (
+                <FaEyeSlash
+                  onClick={handlePasswordShow}
+                  className="cursor-pointer"
+                />
+              ) : (
+                <FaEye
+                  onClick={handlePasswordShow}
+                  className="cursor-pointer"
+                />
+              )}
+            </div>
           </div>
+
+          <button
+            className={` ${
+              checkValues ? "bg-green-800 hover:bg-green-700" : "bg-gray-500"
+            }  text-white w-full py-2 my-5 rounded font-semibold`}
+          >
+            Register
+          </button>
         </form>
       </div>
     </section>
